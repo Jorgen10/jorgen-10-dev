7fb9d7fbf96e01ff2b6c23cc08049034fda7dcb1
Jorgen10 <strabjorgen@gmail.com>
2023-05-04 13:45:43 +0200

    removed something useless

--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,8 @@ export default {
     adapter: adapter({
       // default options are shown. On some platforms
       // these options are set automatically — see below
-      pages: 'docs',
-      assets: 'docs',
+      pages: 'build',
+      assets: 'build',
       fallback: null,
       precompress: false,
       domain: 'private.jorgen-10.no',
